008c8e0a401f3052ae637d2bc493f575fb62e9d5
Jeffrey Banghart <jeffercize@gmail.com>
2026-09-10 10:54:13 -0400

    sample image compute shader

--- gmp/ui/MainMenu.cs
+++ gmp/ui/MainMenu.cs
@@ -1,6 +1,9 @@
 using Godot;
+using Godot.Collections;
 using Godot.NativeInterop;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 // Entry scene. Routes to the shared debug lobby (in LAN or Steam mode), options, or
@@ -23,64 +26,129 @@ public partial class MainMenu : Control
         CallDeferred(nameof(HandleCmdline));
 
         RenderingDevice rd = RenderingServer.CreateLocalRenderingDevice();
-        var shaderFile = GD.Load<RDShaderFile>("res://test.glsl");
-        var shaderBytecode = shaderFile.GetSpirV();
-        var shader = rd.ShaderCreateFromSpirV(shaderBytecode);
-        // Prepare our data. We use floats in the shader, so we need 32 bit.
-        int xSize = 1024;
-        int ySize = 1024;
-        int zSize = 3;
-        float[] floats = new float[xSize*ySize*zSize];
-        for (int z = 0; z < zSize; z++)
+        int x_axis = 4096;
+        int y_axis = 4096;
+        FastNoiseLite noise = new FastNoiseLite();
+        noise.Frequency = 0.005f;
+        //noise.Seed = 1;
+        noise.FractalType = FastNoiseLite.FractalTypeEnum.None;
+        noise.DomainWarpEnabled = false;
+        noise.Offset = new Vector3(0.0f, 0.0f, 0.0f);
+        Stopwatch sw = Stopwatch.StartNew();
+        Image noiseImage = Godot.Image.CreateEmpty(x_axis, y_axis, false, Image.Format.Rf);
+        for(int i = 0; i < x_axis; i++)
         {
-            for (int y = 0; y < ySize; y++)
+            for(int j = 0; j < y_axis; j++)
             {
-                for (int x = 0; x < xSize; x++)
-                {
-                    floats[x + (y * xSize) + (z * xSize * ySize)] = 1;
-                }
+                // Get the noise value at the world position
+                float noiseValue = noise.GetNoise2D(i, j);
+
+                // Set the pixel in the image
+                noiseImage.SetPixel(i, j, new Color(noiseValue, 0, 0, 0));
             }
         }
-        int totalBytes = floats.Length * sizeof(float);
-        byte[] byteArray = new byte[totalBytes];
-        Buffer.BlockCopy(floats, 0, byteArray, 0, totalBytes);
+        sw.Stop();
+        GD.Print("Noise: " + sw.ElapsedMilliseconds);
+        sw.Reset();
+        sw.Start();
+        //setup compute shader
+        var shaderFile = GD.Load<RDShaderFile>("res://test.glsl");
+        RDShaderSpirV blendShaderBytecode = shaderFile.GetSpirV();
+        Rid blendShader = rd.ShaderCreateFromSpirV(blendShaderBytecode);
 
-        // Create a storage buffer that can hold our float values.
-        // Each float has 4 bytes (32 bit) so 10 x 4 = 40 bytes
-        var buffer = rd.StorageBufferCreate((uint)byteArray.Length, byteArray);
+        //Setup Noise Image
+        RDSamplerState noiseSamplerState = new RDSamplerState();
+        Rid noiseSampler = rd.SamplerCreate(noiseSamplerState);
+        RDTextureFormat noiseInputFmt = new RDTextureFormat();
+        noiseInputFmt.Width = (uint)noiseImage.GetWidth();
+        noiseInputFmt.Height = (uint)noiseImage.GetHeight();
+        noiseInputFmt.Format = RenderingDevice.DataFormat.R32Sfloat;
+        noiseInputFmt.UsageBits = RenderingDevice.TextureUsageBits.CanCopyFromBit | RenderingDevice.TextureUsageBits.SamplingBit | RenderingDevice.TextureUsageBits.CanUpdateBit;
+        RDTextureView noiseInputView = new RDTextureView();
+        byte[] noiseInputImageData = noiseImage.GetData();
+        Godot.Collections.Array<byte[]> noiseData = new Godot.Collections.Array<byte[]>
+            {
+                noiseInputImageData
+            };
+        Rid noiseTex = rd.TextureCreate(noiseInputFmt, noiseInputView, noiseData);
+        RDUniform noiseSamplerUniform = new RDUniform();
+        noiseSamplerUniform.UniformType = RenderingDevice.UniformType.SamplerWithTexture;
+        noiseSamplerUniform.Binding = 0;
+        noiseSamplerUniform.AddId(noiseSampler);
+        noiseSamplerUniform.AddId(noiseTex);
 
+        //Setup Output Image 
+        RDTextureFormat blendfmt = new RDTextureFormat();
+        blendfmt.Width = (uint)x_axis;
+        blendfmt.Height = (uint)y_axis;
+        blendfmt.Format = RenderingDevice.DataFormat.R32Sfloat;
+        blendfmt.UsageBits = RenderingDevice.TextureUsageBits.StorageBit | RenderingDevice.TextureUsageBits.CanUpdateBit | RenderingDevice.TextureUsageBits.CanCopyFromBit;
+        RDTextureView blendview = new RDTextureView();
+        Image blend_image = Godot.Image.CreateEmpty(x_axis, y_axis, false, Image.Format.Rf);
+        byte[] blendOutputImageData = blend_image.GetData();
+        Godot.Collections.Array<byte[]> blendTempData = new Godot.Collections.Array<byte[]>
+            {
+                blendOutputImageData
+            };
+        Rid blendOutputTex = rd.TextureCreate(blendfmt, blendview, blendTempData);
+        RDUniform blendOutputTexUniform = new RDUniform();
+        blendOutputTexUniform.UniformType = RenderingDevice.UniformType.Image;
+        blendOutputTexUniform.Binding = 1;
+        blendOutputTexUniform.AddId(blendOutputTex);
 
-        // Create a uniform to assign the buffer to the rendering device
-        var uniform = new RDUniform
+        int imageWidth = noiseImage.GetWidth();
+        int imageHeight = noiseImage.GetHeight();
+        byte[] imageDimensionsBytes = new byte[sizeof(int) * 2];
+        Buffer.BlockCopy(BitConverter.GetBytes(imageWidth), 0, imageDimensionsBytes, 0, sizeof(int));
+        Buffer.BlockCopy(BitConverter.GetBytes(imageHeight), 0, imageDimensionsBytes, sizeof(int), sizeof(int));
+        Rid imageDimensionsBuffer = rd.StorageBufferCreate((uint)imageDimensionsBytes.Length, imageDimensionsBytes);
+
+        RDUniform imageDimensionsUniform = new RDUniform()
         {
             UniformType = RenderingDevice.UniformType.StorageBuffer,
-            Binding = 0
+            Binding = 2
         };
-        uniform.AddId(buffer);
-        var uniformSet = rd.UniformSetCreate([uniform], shader, 0);
+        imageDimensionsUniform.AddId(imageDimensionsBuffer);
+
+        // Create the uniformSet
+        Rid blenduniformSet = rd.UniformSetCreate(new Array<RDUniform> { noiseSamplerUniform, blendOutputTexUniform, imageDimensionsUniform }, blendShader, 0);
 
         // Create a compute pipeline
-        var pipeline = rd.ComputePipelineCreate(shader);
-        var computeList = rd.ComputeListBegin();
-        rd.ComputeListBindComputePipeline(computeList, pipeline);
-        rd.ComputeListBindUniformSet(computeList, uniformSet, 0);
-        rd.ComputeListDispatch(computeList, xGroups: (uint)xSize/32, yGroups: (uint)ySize/32, zGroups: 3);
-        rd.ComputeListEnd();
+        Rid blendpipeline = rd.ComputePipelineCreate(blendShader);
 
-        // Submit to GPU and wait for sync
+        var blendcomputeList = rd.ComputeListBegin();
+        rd.ComputeListBindComputePipeline(blendcomputeList, blendpipeline);
+        rd.ComputeListBindUniformSet(blendcomputeList, blenduniformSet, 0);
+        int blendthreadsPerGroup = 32;
+        uint blendxGroups = (uint)(noiseImage.GetWidth() + blendthreadsPerGroup - 1) / (uint)blendthreadsPerGroup;
+        uint blendyGroups = (uint)(noiseImage.GetHeight() + blendthreadsPerGroup - 1) / (uint)blendthreadsPerGroup;
+        rd.ComputeListDispatch(blendcomputeList, blendxGroups, blendyGroups, 1);
+        rd.ComputeListEnd();
+        sw.Stop();
+        GD.Print("Prep: " + sw.ElapsedMilliseconds);
+        sw.Reset();
+        sw.Start();
         rd.Submit();
         rd.Sync();
 
-        // Read back the data from the buffers
-        var outputBytes = rd.BufferGetData(buffer);
-        var output = new float[xSize * ySize * zSize];
-        Buffer.BlockCopy(outputBytes, 0, output, 0, outputBytes.Length);
-        float total = 0;
-        foreach( var item in output )
-        {
-            total += item;
-        }
-        GD.Print($" expected: {xSize*ySize*zSize*2} vs actual: {total}");
+        sw.Stop();
+        GD.Print("Render: " + sw.ElapsedMilliseconds);
+        sw.Reset();
+        sw.Start();
+        var blendbyteData = rd.TextureGetData(blendOutputTex, 0);
+        sw.Stop();
+        GD.Print("Data Pull: " + sw.ElapsedMilliseconds);
+        sw.Reset();
+        sw.Start();
+        Image final_img = Image.CreateFromData(x_axis, y_axis, false, Image.Format.Rf, blendbyteData);
+        final_img.SavePng("test.png");
+        sw.Stop();
+        GD.Print("Image Save: " + sw.ElapsedMilliseconds);
+        rd.FreeRid(blendShader);
+        rd.FreeRid(noiseSampler);
+        rd.FreeRid(noiseTex);
+        rd.FreeRid(blendOutputTex);
+        rd.FreeRid(imageDimensionsBuffer);
     }
 
     
--- test.glsl
+++ test.glsl
@@ -1,17 +1,22 @@
 #[compute]
 #version 450
 
-// Invocations in the (x, y, z) dimension
-layout(local_size_x = 32, local_size_y = 32, local_size_z = 1) in;
+layout(local_size_x = 32, local_size_y = 32) in;
 
-// A binding to the buffer we create in our script
-layout(set = 0, binding = 0, std430) restrict buffer MyDataBuffer {
-    float data[];
-}
-my_data_buffer;
+// A binding to the uniforms we create in our script
+layout(set = 0, binding = 0) uniform sampler2D noiseTexture;
+layout(set = 0, binding = 1, r32f) uniform image2D outputImage;
+layout(set = 0, binding = 2) restrict buffer ImageDimensions {
+    int imageWidth;
+    int imageHeight;
+};
 
-// The code we want to execute in each invocation
 void main() {
-    // gl_GlobalInvocationID.x uniquely identifies this invocation across all work groups
-    my_data_buffer.data[gl_GlobalInvocationID.x + (gl_GlobalInvocationID.y * 1024) + (gl_GlobalInvocationID.z * 1024 * 1024)] *= 2.0;
+    ivec2 coord = ivec2(gl_GlobalInvocationID.xy);
+    vec2 uv = vec2(coord) / vec2(imageWidth, imageHeight);
+    float height = texture(noiseTexture, uv).r;
+
+    vec4 color = vec4(height, 0.0, 0.0, 1.0);
+
+    imageStore(outputImage, coord, color);
 }
